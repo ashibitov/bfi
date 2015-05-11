5617f996421dc643019cdc7cb95273f955192960
Anthony Shibitov <krgrief@gmail.com>
2015-05-11 03:42:13 -0400

    Added some comments into the main.cpp file. Nothing major.

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-//Comment
+//Brainfuck interpreter written in C++ by Anthony  Shibitov
+//Read .bf code in one character at a time. Run appropriate
+//input output (getchar,  cout). Use  tape  array  of  size
+//30,000 for the tape (of type char).
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+}
