1972e429fa3d6a22d493e11a39c4fdf9b5fa44f8
Anthony Shibitov <krgrief@gmail.com>
2015-05-11 15:44:16 -0400

    Basic interpreter now. Matching braces all work now.

--- main.cpp
+++ main.cpp
@@ -3,9 +3,197 @@
 //input output (getchar,  cout). Use  tape  array  of  size
 //30,000 for the tape (of type char).
 
-#include <iostream>
-using namespace std;
+//Syntax: consists of only ><+-.,[]
+//Everything else is ignored.
 
-int main(){
+//+ : Increments the value at the current cell by one.
+//- : Decrements the value at the current cell by one.
+//> : Moves the data pointer to the next cell (cell on the right).
+//< : Moves the data pointer to the previous cell (cell on the left).
+//. : Prints the ASCII value at the current cell (i.e. 65 = 'A').
+//, : Reads a single input character into the current cell.
+//[ : If the value at the current cell is zero, skips to the corresponding ] .
+//    Otherwise, move to the next instruction.
+//] : If the value at the current cell is zero, move to the next instruction.
+//    Otherwise, move backwards in the instructions to the corresponding [ .
+
+//[ and ] form a while loop. Obviously, they must be balanced.
+//source: learnxinyminutes.com/docs/brainfuck
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define SUCCESS 0
+#define FAILURE 1
+
+#define FALSE false
+#define TRUE true
+
+#define CELLSIZE 30000
+
+void usage(){
+	std::cout << "usage: bfi inputfile.bf (-d) (0-29,999)" << std::endl;
+	std::cout << "\t-d : show debug information\n";
+	std::cout << "\t(0-29,999) : # of cells to display\n\n";
+	return;
+}
+
+bool checkChar(char check){
+	switch(check){
+		case ',':
+			return SUCCESS;
+		case '.':
+			return SUCCESS;
+		case '<':
+			return SUCCESS;
+		case '>':
+			return SUCCESS;
+		case '-':
+			return SUCCESS;
+		case '+':
+			return SUCCESS;
+		case '[':
+			return SUCCESS;
+		case ']':
+			return SUCCESS;
+		default:
+			return FAILURE;
+	}
+}
+
+int main(int argc, char* argv[]){
+	if(argc == 1){
+		usage();
+		return 0;
+	}
+	bool debug = FALSE;
+	int CELL_PRINT = 5;
+	if(argc == 3 || argc == 4){
+		if(strcmp(argv[2], "d")){
+			std::cout << "debug flag active . . .\n";
+			debug = TRUE;
+		}
+		if(argc == 4){
+			CELL_PRINT = atoi(argv[3]);
+		}
+	}
+	std::ifstream inputBF;
+	inputBF.open(argv[1]);
+	if(!inputBF.is_open()){
+		std::cout << "error: file could not be opened" << std::endl;
+		return FAILURE;
+	}
+	std::string filebuffer;
+	std::string fullbuffer;
+	while(std::getline(inputBF, filebuffer)){
+			fullbuffer = fullbuffer + filebuffer;
+	}
+	fullbuffer.erase(std::remove(fullbuffer.begin(), fullbuffer.end(), ' '), fullbuffer.end());
+
+	std::string finalbuffer;
+	for(int i = 0; i < fullbuffer.length(); i++){
+		if(checkChar(fullbuffer[i]) == SUCCESS){
+			finalbuffer = finalbuffer + fullbuffer[i];
+		}
+	}
+	//File has been formatted now. Interpretation can begin.
 	
+	char * tape = new char[CELLSIZE];
+	int tapePosition = 0;
+
+	for(int i = 0; i < finalbuffer.length(); i++){
+		if(debug == TRUE){
+			std::cout << "\n\nON INSTRUCTION " << i << " = " << finalbuffer[i] << "\n";
+			for(int j = 0; j < CELL_PRINT; j++){
+				std::cout << j << "cell = " << (int)tape[j] << std::endl;
+			}
+			std::cout << "\n";
+		}
+		switch(finalbuffer[i]){
+			case '>':
+				tapePosition++;
+				break;
+			case '<':
+				if(tapePosition == 0){
+					break;
+				}
+				tapePosition--;
+				break;
+			case '+':
+				tape[tapePosition]++;
+				break;
+			case '-':
+				if((int)tape[tapePosition] == 0){
+					break;
+				}
+				tape[tapePosition]--;
+				break;
+			case '.':
+				std::cout << tape[tapePosition];
+				break;
+			case ',':
+				std::cin.get(tape[tapePosition]);
+				break;
+			case '[':
+				if(((int)tape[tapePosition]) == 0){
+					//skip to next matching brace
+					int current = i;
+					bool match = TRUE;
+
+					for(int x = i; x < finalbuffer.length(); x++){
+						if(finalbuffer[x] == '['){
+							match = FALSE;
+						}
+						if(finalbuffer[x] == ']' && match == FALSE){
+							if(debug == TRUE)
+								std::cout << "this is another ] brace: " << x << "\n";
+							match = TRUE;
+							continue;
+						}
+						if(finalbuffer[x] == ']' && match == TRUE){
+							i = x;
+							if(debug == TRUE)
+								std::cout << "this is the matching ] brace: " << x << "\n";
+							break;
+						}
+					}
+				}
+				break;
+			case ']':
+				if(((int)tape[tapePosition]) != 0){
+					//skip to prev matching brace
+					int current = i;
+					bool match = TRUE;
+
+					for(int x = i-1; x > 0; x--){
+						if(finalbuffer[x] == ']'){
+							match = FALSE;
+						}
+						if(finalbuffer[x] == '[' && match == FALSE){
+							if(debug == TRUE)
+								std::cout << "this is another [ brace: " << x << "\n";
+							match = TRUE;
+							continue;
+						}
+						if(finalbuffer[x] == '[' && match == TRUE){
+							i = x;
+							if(debug == TRUE)
+								std::cout << "this is the matching [ brace: " << x << "\n";
+							break;
+						}
+					}
+				}
+				break;
+
+			default:
+				break;
+		}
+	}
+
+
+
+
+
+	delete tape;
 }
